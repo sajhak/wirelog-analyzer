cb51bf8a9e04b264d73c08338ca0877876c8dce0
sajhak <sajhak@gmail.com>
2015-04-08 17:19:07 +0530

    First commit, initlial files

---
+++ src/main/java/org/sajith/wirelog/analyzer/App.java
@@ -0,0 +1,13 @@
+package org.sajith.wirelog.analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
